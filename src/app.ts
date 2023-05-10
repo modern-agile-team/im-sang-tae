3fefabe24d894fe30a505be35672e32a60960aca
soonki-98 <alstnsrl98@gmail.com>
2023-05-10 16:18:27 +0900

    예제 수정

--- src/app.ts
+++ src/app.ts
@@ -4,34 +4,26 @@
  * Copyright (c) 2023 Your Company
  */
 
-import { defaultManager } from "./stateManager";
-import { defaultStore } from "./store";
-
-const store = defaultStore;
-const stateManager = defaultManager;
-
-const numberAtom = store.createAtom(1);
-
-const [getNumber, setNumber] = stateManager.getAndSetRSState(numberAtom);
+import { defaultManager as stateManager } from "./stateManager";
+import { defaultStore as store } from "./store";
 
 const Comp = document.createElement("div");
 const Button = document.createElement("button");
 const Text = document.createElement("span");
 
-Button.innerText = "click!";
+const numberAtom = store.createAtom(1);
+const [getNumber, setNumber] = stateManager.getAndSetRSState(numberAtom);
+
+Button.innerText = "+1";
 Text.innerText = `${getNumber()}`;
 
 document.body.appendChild(Comp);
 Comp.appendChild(Button);
 Comp.appendChild(Text);
 
-function rerender<V>(component: HTMLElement, newState: V) {
-  component.innerText = `${newState}`;
-}
-
 Button.onclick = () => {
-  store.subscribeAtom(numberAtom, () => rerender(Text, getNumber()));
   setNumber(getNumber() + 1);
-  store.render(numberAtom);
-  store.unsubscribeAtom(numberAtom, () => rerender(Text, getNumber()));
+  store.rerender(numberAtom, () => {
+    Text.innerText = `${getNumber()}`;
+  });
 };
