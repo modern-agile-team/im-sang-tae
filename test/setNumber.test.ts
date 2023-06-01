aa844629bf4c1a0e2915ed6f66b4395b2f47d8a0
soonki-98 <alstnsrl98@gmail.com>
2023-06-01 15:32:34 +0900

    feat(#1): selector의 get 함수가 promise일때, setAtom의 인자를 awaited로 받을수 있도록 작업

--- test/setNumber.test.ts
+++ test/setNumber.test.ts
@@ -3,7 +3,15 @@ import { defaultStateManger, getDefaultStore } from "../src/index";
 const defaultStore = getDefaultStore();
 
 const numberAtom = defaultStore.createAtom({ key: "numberAtom", initialState: 1 });
+const numberSelector = defaultStore.createAtom({
+  key: "numberSelector",
+  async get({ get }) {
+    return get(numberAtom) + 1;
+  },
+});
+
 const [getNumber, setNumber] = defaultStateManger.atomState(numberAtom);
+const [getNumberS, setNumberS] = defaultStateManger.atomState(numberSelector);
 
 describe("setNumber", () => {
   afterEach(() => {
@@ -12,5 +20,16 @@ describe("setNumber", () => {
 
   test.each([1, 2, 3, 4])("%s", (number: number) => {
     expect(getNumber()).toBe(number);
+  });
+});
+
+describe("setNumberWithAsyncFunction", () => {
+  afterEach(async () => {
+    const number = await getNumberS();
+    setNumberS(number + 1);
+  });
+
+  test.each([6, 7, 8, 9])("%s", async (number: number) => {
+    expect(await getNumberS()).toBe(number);
   });
 });
