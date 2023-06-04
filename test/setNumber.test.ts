61a6853a1fb893d1b3156acb4f02dc147dc34491
soonki-98 <alstnsrl98@gmail.com>
2023-06-04 22:24:13 +0900

    test(#4): 테스트 코드 추가

--- test/setNumber.test.ts
+++ test/setNumber.test.ts
@@ -2,19 +2,36 @@ import { defaultStateManger, getDefaultStore } from "../src/index";
 
 const defaultStore = getDefaultStore();
 
-const numberAtom = defaultStore.createAtom({ key: "numberAtom", initialState: 1 });
+const numberAtom = defaultStore.createAtom({ key: "numberAtom", initialState: 0 });
 const numberSelector = defaultStore.createAtom({
   key: "numberSelector",
   async get({ get }) {
     return get(numberAtom) + 1;
   },
 });
+const numberAtomFamily = defaultStore.createAtomFamily<number, number>({
+  key: "numberAtomFamily",
+  initialState(param) {
+    return 1 + param;
+  },
+});
+
+const numberSelectorFamily = defaultStore.createAtomFamily<number, number>({
+  key: "numberSelectorFamily",
+  get:
+    (param: number) =>
+    ({ get }) => {
+      return get(numberAtom) + param;
+    },
+});
 
 const [getNumber, setNumber] = defaultStateManger.atomState(numberAtom);
 const [getNumberS, setNumberS] = defaultStateManger.atomState(numberSelector);
+const [getNumberAtomFamily, setNumberAtomFamily] = defaultStateManger.atomState(numberAtomFamily(10));
+const [getNumberSelectorFamily, setNumberSelectorFamily] = defaultStateManger.atomState(numberSelectorFamily(10));
 
 describe("setNumber", () => {
-  afterEach(() => {
+  beforeEach(() => {
     setNumber(getNumber() + 1);
   });
 
@@ -24,7 +41,7 @@ describe("setNumber", () => {
 });
 
 describe("setNumberWithAsyncFunction", () => {
-  afterEach(async () => {
+  beforeEach(async () => {
     const number = await getNumberS();
     setNumberS(number + 1);
   });
@@ -35,7 +52,7 @@ describe("setNumberWithAsyncFunction", () => {
 });
 
 describe("setNumberWithCallback", () => {
-  afterEach(async () => {
+  beforeEach(async () => {
     setNumberS(async (prev) => {
       return (await prev) + 1;
     });
@@ -43,5 +60,29 @@ describe("setNumberWithCallback", () => {
 
   test.each([10, 11, 12, 13])("%s", async (number: number) => {
     expect(await getNumberS()).toBe(number);
+  });
+});
+
+describe("setNumberWithAtomFamily", () => {
+  beforeEach(() => {
+    setNumberAtomFamily((prev) => {
+      return prev + 1;
+    });
+  });
+
+  test.each([12, 13, 14, 15])("%s", (number: number) => {
+    expect(getNumberAtomFamily()).toBe(number);
+  });
+});
+
+describe("setNumberWithSelectorFamily", () => {
+  beforeEach(() => {
+    setNumberSelectorFamily((prev) => {
+      return prev + 1;
+    });
+  });
+
+  test.each([15, 16, 17, 18])("%s", (number: number) => {
+    expect(getNumberSelectorFamily()).toBe(number);
   });
 });
