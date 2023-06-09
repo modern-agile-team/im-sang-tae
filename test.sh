42526e443b30fa330788a03a679f876d9f28e931
LeeSeokHo <rrgks@naver.com>
2023-06-09 18:55:05 +0900

    0.5.19

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 VERSION_BUMP="patch"
 
-NEW_VERSION=$(npm version $VERSION_BUMP) 
-
-echo "------------"
-echo $NEW_VERSION
-echo "------------"
-
+NEW_VERSION="0.5.19"
+echo "NEW_VERSION: $NEW_VERSION"
+git remote -v
+git status
 git add .
-
 git commit -m "$NEW_VERSION"
+git status
+git push origin feat/shlee/62
+git status
